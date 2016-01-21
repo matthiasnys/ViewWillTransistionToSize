93023f06347140b2e8c6eebb836dfbbc49f8ccf5
Matthias Nys <matthias@b-nys.com>
2016-01-21 14:38:38 +0100

    Initial Commit

---
+++ ViewWillTransistionToSize/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ViewWillTransistionToSize
+//
+//  Created by Matthias Nys on 21/01/16.
+//  Copyright © 2016 B-NYS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
